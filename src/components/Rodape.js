bd924518fb5fa87b498bff46cb7b4a3acc21c746
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-08-12 02:29:33 -0300

    Arrumando layout de Rodapé e página Inicial

---
+++ src/components/Rodape.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rodape = () =>{
+    return(
+        <section id="rodape">
+            <a>Quero Participar</a>
+            <a>Como funciona</a>
+            <section>
+                <p>Dúvida ou sugestão?</p>
+                <p>suporte@bancodetempo.com</p>
+            </section>
+        </section>
+    )
+}
+
+export default Rodape;
