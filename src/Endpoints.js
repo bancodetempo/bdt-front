363f61e240f55f7d4022163d7fe737ee0ca1cf14
Davi A. Wasserberg <davi.abreu@magnetis.com.br>
2020-09-04 03:03:10 -0300

    Add Typeahead for autocomplete feature.

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -1,5 +1,6 @@
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const Endpoints = {
-  orders: backendUrl + '/api/v0/orders/'
+  orders: backendUrl + '/api/v0/orders/',
+  users: backendUrl + '/api/v0/users/?search='
 };
