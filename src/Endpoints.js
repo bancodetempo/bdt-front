cb1dbfa9d6f32e3e8cf5c0d2e15280638a00cd24
Davi A. Wasserberg <davi.abreu@magnetis.com.br>
2020-10-03 02:46:31 -0300

    Voltando backendUrl para variável de ambiente.

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -1,4 +1,4 @@
-const backendUrl = 'https://bdt-backend.herokuapp.com';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const Endpoints = {
   orders: backendUrl + '/api/v0/orders/',
